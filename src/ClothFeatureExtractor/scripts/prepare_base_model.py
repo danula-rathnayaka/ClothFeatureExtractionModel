2dc19f236b6ef9d36bc3b1b57e794f2474f0c7b9
Danula <danusathmin@gmail.com>
2025-05-24 00:26:49 +0530

    update prepare base model

--- src/ClothFeatureExtractor/scripts/prepare_base_model.py
+++ src/ClothFeatureExtractor/scripts/prepare_base_model.py
@@ -10,7 +10,7 @@ class PrepareBaseModel:
         self.config = config
 
     def get_base_model(self):
-        self.model = tf.keras.applications.vgg16.VGG16(
+        self.model = tf.keras.applications.EfficientNetB0(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
             include_top=self.config.params_include_top
@@ -23,23 +23,27 @@ class PrepareBaseModel:
         if freeze_all:
             for layer in model.layers:
                 layer.trainable = False
-        elif (freeze_till is not None) and (freeze_till > 0):
+        elif freeze_till is not None and freeze_till > 0:
             for layer in model.layers[:-freeze_till]:
                 layer.trainable = False
+            for layer in model.layers[-freeze_till:]:
+                layer.trainable = True
+        else:
+            for layer in model.layers:
+                layer.trainable = True
 
-        flatten_in = tf.keras.layers.Flatten()(model.output)
-        prediction = tf.keras.layers.Dense(
-            units=classes,
-            activation="softmax"
-        )(flatten_in)
+        x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dropout(0.4)(x)
+        x = tf.keras.layers.Dense(256, activation='relu')(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dropout(0.3)(x)
+        output = tf.keras.layers.Dense(classes, activation='softmax')(x)
 
-        full_model = tf.keras.models.Model(
-            inputs=model.input,
-            outputs=prediction
-        )
+        full_model = tf.keras.models.Model(inputs=model.input, outputs=output)
 
         full_model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
             loss=tf.keras.losses.CategoricalCrossentropy(),
             metrics=["accuracy"]
         )
@@ -47,13 +51,17 @@ class PrepareBaseModel:
         full_model.summary()
         return full_model
 
-    def update_base_model(self):
+    def update_base_model(self, freeze_all=True, freeze_till=None, learning_rate=None):
+        if learning_rate is None:
+            learning_rate = self.config.params_learning_rate
+
         full_model = self._prepare_full_model(
             model=self.model,
             classes=self.config.params_classes,
-            freeze_all=True,
-            freeze_till=None,
-            learning_rate=self.config.params_learning_rate
+            freeze_all=freeze_all,
+            freeze_till=freeze_till,
+            learning_rate=learning_rate
         )
 
         save_model(path=self.config.updated_base_model_path, model=full_model)
+        return full_model
